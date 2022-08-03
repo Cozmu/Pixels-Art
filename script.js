4e3a752343045dbaa59319adccd37aa8a71607b2
Cozmu <www.jorgereis@gmail.com>
2022-08-03 00:45:51 -0300

    Altomatizando cores de paleta

--- script.js
+++ script.js
@@ -9,19 +9,35 @@ function criaTabela() {
 }
 criaTabela();
 //-------------------------------------------------------------
+function pintarModelos() {
+    let modelos = document.querySelectorAll('.color')
+    for (let index = 0; index < modelos.length; index += 1) {
+        if (modelos[index] === modelos[0]) {
+            modelos[index].style.backgroundColor = 'black';
+        } else if (modelos[index] === modelos[1]) {
+            modelos[index].style.backgroundColor = 'blue';
+        } else if (modelos[index] === modelos[2]) {
+            modelos[index].style.backgroundColor = 'green';
+        } else {
+            modelos[index].style.backgroundColor = 'red';
+        }
+    }
+}
+pintarModelos();
+
+//--------------------------------------------------------------
 let receptorBlack = document.querySelectorAll('.color')[0]
 let receptorBlue = document.querySelectorAll('.color')[1]
 let receptorGreen = document.querySelectorAll('.color')[2]
 let receptorRed = document.querySelectorAll('.color')[3]
-
 /* let receptor = document.querySelector('.color') */
 
 function seleciona(event) {
     /* for (let index = 0; index < receptor.length; index += 1) { */
-        let selecionado = document.querySelector('.selected')
-        selecionado.classList.remove('selected')
-        event.target.classList.add('selected')
-   /*  } */
+    let selecionado = document.querySelector('.selected')
+    selecionado.classList.remove('selected')
+    event.target.classList.add('selected')
+    /*  } */
 
 }
 /* receptor.addEventListener('click', seleciona); */
@@ -29,7 +45,7 @@ function seleciona(event) {
 receptorBlack.addEventListener('click', seleciona);
 receptorBlue.addEventListener('click', seleciona);
 receptorGreen.addEventListener('click', seleciona);
-receptorRed.addEventListener('click', seleciona); 
+receptorRed.addEventListener('click', seleciona);
 //-------------------------------------------------------------
 
 
