0353461ce58bebe6446c5e5823c55ae30cd99ad5
Cozmu <www.jorgereis@gmail.com>
2022-12-21 18:17:55 -0300

    style: corrigindo identação

--- script.js
+++ script.js
@@ -1,76 +1,77 @@
-let imputNumber = document.getElementById('board-size');
+const imputNumber = document.getElementById('generate-board');
 
-
-
-
-
-
-
-
-function criaTabela() {
-    let paiTabela = document.getElementById('pixel-board');
-    for (let index = 1; index <= 25; index += 1) {
-        let div = document.createElement('div');
-        div.className = 'pixel';
-        paiTabela.appendChild(div);
-    }
+function tamanhoTabela() {
+  const numero = document.getElementById('board-size');
+  const vazio = '';
+  if (numero.value === vazio) {
+    alert('Board inválido!');
+  }
 }
-criaTabela();
+imputNumber.addEventListener('click', tamanhoTabela);
+
+function criaTabela(number) {
+  const paiTabela = document.getElementById('pixel-board');
+  for (let index = 1; index <= number; index += 1) {
+    const div = document.createElement('div');
+    div.className = 'pixel';
+    paiTabela.appendChild(div);
+  }
+}
+criaTabela(25);
 
 //-------------------------------------------------------------
 function pintarModelos() {
-    let modelos = document.querySelectorAll('.color')
-    for (let index = 0; index < modelos.length; index += 1) {
-        if (modelos[index] === modelos[0]) {
-            modelos[index].style.backgroundColor = 'black';
-        } else if (modelos[index] === modelos[1]) {
-            modelos[index].style.backgroundColor = 'blue';
-        } else if (modelos[index] === modelos[2]) {
-            modelos[index].style.backgroundColor = 'green';
-        } else if (modelos[index] === modelos[3]) {
-            modelos[index].style.backgroundColor = 'red';
-        }
+  const modelos = document.querySelectorAll('.color');
+  for (let index = 0; index < modelos.length; index += 1) {
+    if (modelos[index] === modelos[0]) {
+      modelos[index].style.backgroundColor = 'black';
+    } else if (modelos[index] === modelos[1]) {
+      modelos[index].style.backgroundColor = 'blue';
+    } else if (modelos[index] === modelos[2]) {
+      modelos[index].style.backgroundColor = 'green';
+    } else {
+      modelos[index].style.backgroundColor = 'red';
     }
+  }
 }
+
 pintarModelos();
 
 //--------------------------------------------------------------
 function selecionar(muda) {
-    let receptor = document.querySelectorAll('.color');
-    for (let index = 0; index < receptor.length; index += 1) {
-        let selecionado = document.querySelector('.selected');
-        if (selecionado === receptor[index]) {
-            selecionado.classList.remove('selected');
-            muda.target.classList.add('selected');
-        }
+  const receptor = document.querySelectorAll('.color');
+  for (let index = 0; index < receptor.length; index += 1) {
+    const selecionado = document.querySelector('.selected');
+    if (selecionado === receptor[index]) {
+      selecionado.classList.remove('selected');
+      muda.target.classList.add('selected');
     }
+  }
 }
 
-let receptor = document.querySelectorAll('.color')
+const receptor = document.querySelectorAll('.color');
 for (let index = 0; index < receptor.length; index += 1) {
-    receptor[index].addEventListener('click', selecionar)
-
+  receptor[index].addEventListener('click', selecionar);
 }
 
 //-------------------------------------------------------------
-function pintar(event) {
-    let corArmazenada = document.getElementsByClassName('selected')[0]
-    event.target.style.backgroundColor = corArmazenada.style.backgroundColor;
+function pintar({ target }) {
+  const corArmazenada = document.getElementsByClassName('selected')[0];
+  target.style.backgroundColor = corArmazenada.style.backgroundColor;
 }
 
-
-let pixel = document.querySelectorAll('.pixel')
+const pixel = document.querySelectorAll('.pixel');
 for (let index = 0; index < pixel.length; index += 1) {
-    pixel[index].addEventListener('click', pintar)
+  pixel[index].addEventListener('click', pintar);
 }
 //-------------------------------------------------------------
-function reset (event) {
-    let pixel = document.querySelectorAll('.pixel')
-    for (let index = 0; index < pixel.length; index += 1) {
-        pixel[index].style.backgroundColor = 'white';
-    }
+function reset() {
+  const pixelReset = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixelReset.length; index += 1) {
+    pixelReset[index].style.backgroundColor = 'white';
+  }
 }
 
-let botao = document.getElementById('clear-board')
-botao.addEventListener('click', reset)
+const botao = document.getElementById('clear-board');
+botao.addEventListener('click', reset);
 //---------------------------------------------------------------
